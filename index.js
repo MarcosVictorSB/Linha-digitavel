68fd438317f3ed11d3bdfe899401e671cd7cc3f3
Marcos Victor <marcos@conveniar.com.br>
2019-10-02 16:57:42 -0300

    limitando a quantidade de caracteres

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-   $("#valor").mask('000.000.000.000.000,00', {reverse: true});
+   $("#valor").mask('000.000.000,00', {reverse: true});
     new ClipboardJS('#button-addon2');
 });
 
